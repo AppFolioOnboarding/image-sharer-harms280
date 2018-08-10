14adf88cae9cb19c42c7fa8512b6ce89852c06b6
Aaron Harms <aaron.harms@appfolio.com>
2018-08-10 11:29:33 -0700

    ah - Add url validation to image link field and tests

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -13,4 +13,34 @@ class ImageTest < ActiveSupport::TestCase
     assert_not_predicate image, :valid?
     assert_equal "can't be blank", image.errors.messages[:link].first
   end
+
+  def test_image__valid_if_link_is_valid_url
+    valid_urls = %w[https://www.massinsight.org/wp-content/uploads/2016/05/placeholder-4-500x300.png
+                    https://www.massinsight.org http://www.massinsight.org https://massinsight.org]
+
+    images = []
+
+    valid_urls.each do |url|
+      images.push(Image.new(link: url))
+    end
+
+    images.each do |img|
+      assert_predicate img, :valid?
+      assert_not_predicate img.errors.messages[:link], :any?
+    end
+  end
+
+  def test_image__invalid_if_link_is_not_valid_url
+    invalid_urls = %w[https://www.massinsight https://https://www.massinsight.com http://]
+    images = []
+
+    invalid_urls.each do |url|
+      images.push(Image.new(link: url))
+    end
+
+    images.each do |img|
+      assert_not_predicate img, :valid?
+      assert_equal 'invalid URL. Link requires http or https', img.errors.messages[:link].first
+    end
+  end
 end

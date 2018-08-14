bb971b3fb2fc749fbd435a6b35bfc5052b66f990
Aaron Harms <aaron.harms@appfolio.com>
2018-08-14 16:44:47 -0700

    ah - Create helper method in application controller to properly add active class to nav links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :add_active_class
+
   protect_from_forgery with: :exception
+
+  def add_active_class(*actions)
+    actions.include?(action_name) ? 'active' : ''
+  end
 end

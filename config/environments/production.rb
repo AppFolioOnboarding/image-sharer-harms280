24cb4b8d04b34ff04f6203bca82c9619f6e22e9b
Aaron Harms <aaron.harms@appfolio.com>
2018-09-06 16:00:35 -0700

    ah - Ignore block length metric in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/BlockLength
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
@@ -101,3 +102,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+# rubocop:enable Metrics/BlockLength
